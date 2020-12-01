ce7964a0b0cd2d3cb149c9b36aaf9a0f97e3fe78
Ethan Bailey <ebailey256@gmail.com>
2020-12-01 14:39:07 -0500

    fixed for loop

--- controller.lua
+++ controller.lua
@@ -2,7 +2,7 @@ local url = 'https://raw.githubusercontent.com/ebai101/minecraftbots/master/mine
 
 local function start()
   local directions = {'front', 'back', 'left', 'right'}
-  for i, #directions do
+  for i = 1, #directions do
     peripheral.call(directions[i], 'wget run ' .. url)
   end
 end
