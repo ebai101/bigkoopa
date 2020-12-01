07f076fb5d51e1988e5b480a7bb280959784bce9
Ethan Bailey <ebailey256@gmail.com>
2020-12-01 14:12:19 -0500

    new implementation

--- minebot.lua
+++ minebot.lua
@@ -1,191 +1,98 @@
-local SLOT_COUNT = 16
-local d = "north"
-local width, depth, height = 10, 10, 2
+turtle = nil
 
+local SLOT_COUNT = 16
+local d = 1
+local width, depth = 10, 10
 if (#arg == 3) then
-    width = tonumber(arg[1])
-    depth = tonumber(arg[2])
-    height = tonumber(arg[3])
+  width = tonumber(arg[1])
+  depth = tonumber(arg[2])
 else
-    print('None or Malformed Size Given, Defaulting to 10x10x2 up')
+  print('invalid size given, defaulting to 10x10')
 end
 
 DROPPED_ITEMS = {
-    "minecraft:stone",
-    "minecraft:dirt",
-    "minecraft:cobblestone",
-    "minecraft:sand",
-    "minecraft:gravel",
-    "minecraft:redstone",
-    "minecraft:flint",
-    "railcraft:ore_metal",
-    "extrautils2:ingredients",
-    "minecraft:dye",
-    "thaumcraft:nugget",
-    "thaumcraft:crystal_essence",
-    "thermalfoundation:material",
-    "projectred-core:resource_item",
-    "thaumcraft:ore_cinnabar",
-    "deepresonance:resonating_ore",
-    "forestry:apatite"
+  'minecraft:stone',
+  'minecraft:dirt',
+  'minecraft:cobblestone',
+  'minecraft:sand',
+  'minecraft:gravel',
+  'minecraft:redstone',
+  'minecraft:flint',
+  'railcraft:ore_metal',
+  'extrautils2:ingredients',
+  'minecraft:dye',
+  'thaumcraft:nugget',
+  'thaumcraft:crystal_essence',
+  'thermalfoundation:material',
+  'projectred-core:resource_item',
+  'thaumcraft:ore_cinnabar',
+  'deepresonance:resonating_ore',
+  'forestry:apatite'
 }
 
-function dropItems()
-    print("Purging Inventory...")
-    for slot = 1, SLOT_COUNT, 1 do
-        local item = turtle.getItemDetail(slot)
-        if(item ~= nil) then
-            for filterIndex = 1, #DROPPED_ITEMS, 1 do
-                if(item["name"] == DROPPED_ITEMS[filterIndex]) then
-                    print("Dropping - " .. item["name"])
-                    turtle.select(slot)
-                    turtle.dropDown()
-                end
-            end
+local function dropItems()
+  print("Purging Inventory...")
+  for slot = 1, SLOT_COUNT, 1 do
+    local item = turtle.getItemDetail(slot)
+    if(item ~= nil) then
+      for filterIndex = 1, #DROPPED_ITEMS, 1 do
+        if(item['name'] == DROPPED_ITEMS[filterIndex]) then
+          print('dropping - ' .. item['name'])
+          turtle.select(slot)
+          turtle.dropDown()
         end
+      end
     end
+  end
 end
 
-function getEnderIndex()
+local function checkFuel()
+  turtle.select(1)
+  if(turtle.getFuelLevel() < 50) then
+    print('attempting refuel')
     for slot = 1, SLOT_COUNT, 1 do
-        local item = turtle.getItemDetail(slot)
-        if(item ~= nil) then
-            if(item["name"] == "enderstorage:ender_storage") then
-                return slot
-            end
-        end
-    end
-    return nil
-end
-
-function manageInventory()
-    dropItems()
-    index = getEnderIndex()
-    if(index ~= nil) then
-        turtle.select(index)
-        turtle.digUp()
-        turtle.placeUp()
-    end
-    -- Chest is now deployed
-    for slot = 1, SLOT_COUNT, 1 do
-        local item = turtle.getItemDetail(slot)
-        if(item ~= nil) then
-            if(item["name"] ~= "minecraft:coal_block" and item["name"] ~= "minecraft:coal") then
-                turtle.select(slot)
-                turtle.dropUp()
-            end
-        end
-    end
-    -- Items are now stored
-
-    turtle.digUp()
-end
-
-function checkFuel()
-    turtle.select(1)
-
-    if(turtle.getFuelLevel() < 50) then
-        print("Attempting Refuel...")
-        for slot = 1, SLOT_COUNT, 1 do
-            turtle.select(slot)
-            if(turtle.refuel(1)) then
-                return true
-            end
-        end
-
-        return false
-    else
+      turtle.select(slot)
+      if(turtle.refuel(1)) then
         return true
+      end
     end
+    return false
+  end
+  return true
 end
 
-
-function detectAndDig()
-    while(turtle.detect()) do
-        turtle.dig()
-        turtle.digUp()
-        turtle.digDown()
-    end
-end
-
-function forward()
-    detectAndDig()
-    turtle.forward()
-end
-
-function leftTurn()
+local function nextCol(col)
+  if col % 2 == 1 then
+    turtle.turnRight()
+    digForward()
+    turtle.turnRight()
+  else
     turtle.turnLeft()
-    detectAndDig()
-    turtle.forward()
+    digForward()
     turtle.turnLeft()
-    detectAndDig()
+  end
 end
 
-
-function rightTurn()
-    turtle.turnRight()
-    detectAndDig()
+local function digForward()
+  if turtle.detect() then 
+    turtle.dig()
     turtle.forward()
-    turtle.turnRight()
-    detectAndDig()
+  end
+  if turtle.detectUp() then turtle.digUp() end
+  if turtle.detectDown() then turtle.digDown() end
 end
 
-function flipDirection()
-    if(d == "north") then
-        d = "south"
-    elseif(d == "south") then
-        d = "north"
-    elseif(d == "west") then
-        d = "east"
-    elseif(d == "east") then
-        d = "west"
+local function main()
+  for col = 1, width do
+    for row = 1, depth - 1 do
+      if not checkFuel() then
+        print('out of fuel')
+        return
+      end
+      digForward()
+      print(string.format("Row: %d   Col: %d", row, col))
     end
-
+    if col ~= width then nextCol(col) end
+  end
+  print('done!')
 end
-
-function turnAround(tier)
-    if(tier % 2 == 1) then
-        if(d == "north" or d == "east") then
-            rightTurn()
-        elseif(d == "south" or d == "west") then
-            leftTurn()
-        end
-    else
-        if(d == "north" or d == "east") then
-            leftTurn()
-        elseif(d == "south" or d == "west") then
-            rightTurn()
-        end
-    end
-    flipDirection()
-end
-
-function riseTier()
-    turtle.turnRight()
-    turtle.turnRight()
-    flipDirection()
-    turtle.digUp()
-    turtle.up()
-end
-
-function start()
-    for tier = 1, height, 1 do
-        for col = 1, width, 1 do
-            for row = 1, depth - 1, 1 do
-                if(not checkFuel()) then
-                    print("Turtle is out of fuel, Powering Down...")
-                    return
-                end
-                forward()
-                print(string.format("Row: %d   Col: %d", row, col))
-            end
-            if(col ~= width) then
-                turnAround(tier)
-            end
-            manageInventory()
-        end
-        riseTier()
-    end
-end
-
-start()
