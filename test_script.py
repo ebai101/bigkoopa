77245853d6944592d5c4d90d38e4074ad0ac34ae
Ethan Bailey <ebailey256@gmail.com>
2021-01-01 01:19:27 -0500

    added refuel from chest

--- test_script.py
+++ test_script.py
@@ -4,13 +4,12 @@ from turtleswarm import swarm, api, util
 
 def test(t: api.Turtle):
 
-    t.peripheral_get_names()
-
-    # util.clear_cave(t, 50, api.LEFT)
+    util.refuel_from_chest(t, 32)
+    util.clear_cave(t, 49, api.LEFT)
 
 
 # create a new swarm with test() as the target
 s = swarm.TurtleSwarm(test, 16)
 
 # run the target on all turtles
-s.run(log_level=logging.DEBUG)
+s.run(log_level=logging.INFO)
--- turtleswarm/util.py
+++ turtleswarm/util.py
@@ -2,34 +2,61 @@ from turtleswarm import api
 
 
 # attempts to refuel the turtle using objects from the turtle's inventory
-def refuel_from_inventory(t: api.Turtle, amount=None) -> bool:
-    fuel_level = t.get_fuel_level()
+def refuel_from_inventory(t: api.Turtle, goal_fuel_count: int = 0) -> bool:
+    init_fuel_count = t.get_fuel_level()
     last_slot = t.get_selected_slot()
 
     # if we don't need fuel, consider it a success
-    if fuel_level == -1:
+    if init_fuel_count == -1:
         return True
 
     # if no amount specified, fill as much as possible
-    if amount == None:
-        amount = t.get_fuel_limit()
+    if goal_fuel_count == 0:
+        goal_fuel_count = t.get_fuel_limit()
 
-    while fuel_level < amount:
+    while init_fuel_count < goal_fuel_count:
         for n in range(1, 17):
             if t.get_item_count(n) > 0:
                 t.select(n)
                 if t.refuel(1):
                     # this item is usable fuel, keep refueling
                     while t.get_item_count(
-                            n) > 0 and t.get_fuel_level() < amount:
+                            n) > 0 and t.get_fuel_level() < goal_fuel_count:
                         t.refuel(1)
-                    if t.get_fuel_level() >= amount:
+                    if t.get_fuel_level() >= goal_fuel_count:
                         # fueled up
                         t.select(last_slot)
                         return True
         t.select(last_slot)
         return False
     return True
+
+
+# attempts to refuel the turtle from an adjacent chest
+# the chest should be the only adjacent peripheral, and should only contain fuel
+def refuel_from_chest(t: api.Turtle, item_count: int = 0) -> bool:
+    side = t.peripheral_get_names()[0]
+
+    if side == 'left':
+        t.turn_left()
+        t.suck(item_count)
+        t.turn_right()
+    elif side == 'right':
+        t.turn_right()
+        t.suck(item_count)
+        t.turn_left()
+    elif side == 'back':
+        t.turn_around()
+        t.suck(item_count)
+        t.turn_around()
+    elif side == 'front':
+        t.suck(item_count)
+    elif side == 'top':
+        t.suck_up(item_count)
+    elif side == 'bottom':
+        t.suck_down(item_count)
+
+    return refuel_from_inventory(t)
 
 
 # excavates a hole of dim (x,y,z)
