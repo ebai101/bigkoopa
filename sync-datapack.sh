079261707731a480cd537187d9521adf0c19df1e
Ethan Bailey <ebailey256@gmail.com>
2021-01-01 02:20:46 -0500

    sync ccemux as well

--- sync-datapack.sh
+++ sync-datapack.sh
@@ -1,2 +1,4 @@
 target_save=$1
+
 rsync -avu --delete "turtleswarm-client" "$HOME/Library/ApplicationSupport/minecraft/saves/$target_save/datapacks"
+ln -sf "$(pwd)/turtleswarm-client/data" "$HOME/Library/ApplicationSupport/ccemux/assets"
