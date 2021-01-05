60d2551017b8253f1c9fd64ea053fa22ff1685bd
Ethan Bailey <ebailey256@gmail.com>
2021-01-05 18:36:35 -0500

    moved to 1.12 format

--- test_script.py
+++ test_script.py
@@ -1,10 +1,15 @@
 from turtleswarm import swarm, api, util
 
 
-def digg(t: api.Turtle):
-    util.spawner(t)
-    # t.refuel()
-    # util.fast_excavate(t, (4, 256, 4), api.LEFT)
+def fast(t: api.Turtle):
+    # util.spawner(t)
+    t.refuel()
+    util.fast_excavate(t, (4, 256, 4), api.LEFT)
+
+
+def slow(t: api.Turtle):
+    t.refuel()
+    util.excavate(t, (4, 256, 4), api.LEFT)
 
 
 s = swarm.TurtleSwarm(digg, 16)
